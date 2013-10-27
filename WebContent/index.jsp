ddcb3de7e94ef06d628a6629ca461b6f8cc0bbad
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-10-27 22:32:58 +0100

    Icefaces added

---
+++ WebContent/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+	<%
+		response.sendRedirect("./console.jsf");
+	%>
+</body>
+</html>
---
+++ src/org/monitor/viewmodels/ConsoleViewModel.java
@@ -0,0 +1,11 @@
+package org.monitor.viewmodels;
+
+/**
+ * Managed Bean of the console view
+ * 
+ * @author khalil.amdouni
+ *
+ */
+public class ConsoleViewModel {
+
+}
