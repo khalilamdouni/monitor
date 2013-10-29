ce75aa8fe6486134a5d227764df78ba4b634a875
Khalil AMDOUNI <amdounikhalil@gmail.com>
2013-10-29 22:24:34 +0100

    Chart support added

--- src/org/monitor/viewmodels/ConsoleViewModel.java
+++ src/org/monitor/viewmodels/ConsoleViewModel.java
@@ -1,20 +1,25 @@
 package org.monitor.viewmodels;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import org.icefaces.ace.component.chart.Axis;
+import org.icefaces.ace.component.chart.AxisType;
 import org.icefaces.ace.event.DateSelectEvent;
+import org.icefaces.ace.model.chart.CartesianSeries;
 import org.monitor.services.impl.ConsoleService;
 
 /**
  * Managed Bean of the console view
  * 
  * @author khalil.amdouni
- *
+ * 
  */
 @ManagedBean
 @ViewScoped
@@ -27,6 +32,65 @@ public class ConsoleViewModel implements Serializable {
 
 	private Date selectDateProperty = new Date(System.currentTimeMillis());
 
+	public static final String BEAN_NAME = "chartLineBean";
+
+	private List<CartesianSeries> lineData = new ArrayList<CartesianSeries>() {
+		{
+			add(new CartesianSeries() {
+				{
+					add("Nickle", 28);
+					add("Aluminum", 13);
+					add("Xenon", 54);
+					add("Silver", 47);
+					add("Sulfur", 16);
+					add("Silicon", 14);
+					add("Vanadium", 23);
+					setLabel("Resources / Demand");
+				}
+			});
+		}
+	};
+
+	private Axis[] yAxes = new Axis[] { new Axis() {
+		{
+			setAutoscale(true);
+			setTickInterval("5");
+			setLabel("Tonnes");
+		}
+	} };
+
+	private Axis xAxis = new Axis() {
+		{
+			setTicks(new String[] { "Nickle", "Aluminum", "Xenon", "Silver",
+					"Sulfur", "Silicon", "Vanadium" });
+			setType(AxisType.CATEGORY);
+		}
+	};
+
+	public List<CartesianSeries> getLineData() {
+		return lineData;
+	}
+
+	public void setLineData(List<CartesianSeries> lineData) {
+		this.lineData = lineData;
+	}
+
+	public Axis[] getyAxes() {
+		return yAxes;
+	}
+
+	public void setyAxes(Axis[] yAxes) {
+		this.yAxes = yAxes;
+	}
+
+	public Axis getxAxis() {
+		return xAxis;
+	}
+
+	public void setxAxis(Axis xAxis) {
+		this.xAxis = xAxis;
+	}
+
 	public Date getSelectDateProperty() {
 		return selectDateProperty;
 	}
@@ -34,12 +98,12 @@ public class ConsoleViewModel implements Serializable {
 	public void setSelectDateProperty(Date selectDateProperty) {
 		this.selectDateProperty = selectDateProperty;
 	}
-	
+
 	public void dateSelectListener(DateSelectEvent event) {
 		consoleService.getJVMStatus();
 		setSelectDateProperty(event.getDate());
-    }
-	
+	}
+
 	public ConsoleService getConsoleService() {
 		return consoleService;
 	}
@@ -47,5 +111,5 @@ public class ConsoleViewModel implements Serializable {
 	public void setConsoleService(ConsoleService consoleService) {
 		this.consoleService = consoleService;
 	}
-	
+
 }
